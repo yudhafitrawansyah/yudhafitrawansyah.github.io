7323d79bbb406fb335fb12d952c9978fdc02e4d1
yudhafitrawansyah <yfitrawansyah@gmail.com>
2026-07-24 16:15:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -49,3 +49,25 @@ window.addEventListener("scroll", () => {
     }
 
 });
+
+// ============================
+// Scroll Reveal Animation
+// ============================
+
+const hiddenElements = document.querySelectorAll(".hidden");
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+});
+
+hiddenElements.forEach((el)=>observer.observe(el));
