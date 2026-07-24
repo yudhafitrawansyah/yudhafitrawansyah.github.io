85ec304d5a039958d8a23285a3ebcb12a3db9b74
yudhafitrawansyah <yfitrawansyah@gmail.com>
2026-07-24 16:02:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -33,3 +33,19 @@ window.addEventListener("scroll", () => {
     });
 
 });
+
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 80){
+
+        header.classList.add("sticky");
+
+    }else{
+
+        header.classList.remove("sticky");
+
+    }
+
+});
