fa8629547d9cd43812e6555efe3bba432768384c
yudhafitrawansyah <yfitrawansyah@gmail.com>
2026-07-24 16:01:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 // Main JavaScript
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 120;
+
+        const sectionHeight = section.clientHeight;
+
+        if(pageYOffset >= sectionTop){
+
+            current = section.getAttribute("id");
+
+        }
+
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if(link.getAttribute("href") === "#" + current){
+
+            link.classList.add("active");
+
+        }
+
+    });
+
+});
