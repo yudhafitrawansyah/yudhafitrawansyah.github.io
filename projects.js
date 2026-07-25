5f1147700d31d8fcf1f0b4f70a0d7c8b2f869aed
yudhafitrawansyah <yfitrawansyah@gmail.com>
2026-07-25 16:32:58 +0800

    Create projects.js

---
+++ projects.js
@@ -0,0 +1,34 @@
+const projects = [
+    {
+        id: 1,
+
+        title: "Weekly Meeting",
+
+        image: "assets/project1.jpg",
+
+        project: "AMMAN Process Expansion Project",
+
+        company: "PT Pengembangan Industri Logam",
+
+        role: "Civil Engineer | QA/QC Engineer",
+
+        location: "Sumbawa Barat, Indonesia",
+
+        duration: "2024 - Present",
+
+        description:
+            "Participated in weekly engineering meetings to coordinate construction progress, resolve technical issues, and monitor project execution.",
+
+        responsibilities: [
+            "Attend weekly coordination meetings",
+            "Review construction progress",
+            "Coordinate with contractors",
+            "Prepare engineering documentation",
+            "Support QA/QC activities"
+        ],
+
+        gallery: [
+            "assets/project1.jpg"
+        ]
+    }
+];
