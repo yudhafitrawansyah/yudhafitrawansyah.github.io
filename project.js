a168fd2d8cb975068f449b22de21fbbbb3368c2a
yudhafitrawansyah <yfitrawansyah@gmail.com>
2026-07-25 16:33:37 +0800

    Create project.js

---
+++ project.js
@@ -0,0 +1,7 @@
+const project = projects[0];
+
+document.getElementById("project-title").textContent = project.title;
+
+document.getElementById("project-image").src = project.image;
+
+document.getElementById("project-description").textContent = project.description;
