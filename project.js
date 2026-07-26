271f5a2bbaaa4d8001a9672bfcd00a10c5d4668d
yudhafitrawansyah <yfitrawansyah@gmail.com>
2026-07-26 09:42:46 +0800

    Update project.js

--- project.js
+++ project.js
@@ -8,15 +8,12 @@ const projectId = Number(params.get("id"));
 const project = projects.find(item => item.id === projectId);
 
 if (!project) {
-
     document.body.innerHTML = `
-        <h1 style="text-align:center;margin-top:100px;">
+        <h1 style="text-align:center; margin-top:100px;">
             Project Not Found
         </h1>
     `;
-
     throw new Error("Project not found");
-
 }
 
 document.getElementById("project-title").textContent = project.title;
