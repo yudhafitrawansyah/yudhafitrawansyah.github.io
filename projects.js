74f9fb674c16c50948081cf4248d78b2ecd708de
yudhafitrawansyah <yfitrawansyah@gmail.com>
2026-07-26 11:49:19 +0800

    Update projects.js

--- projects.js
+++ projects.js
@@ -8,7 +8,7 @@ const projects = [
         role: "Civil Engineer | QA/QC Engineer",
         location: "Sumbawa Barat, Indonesia",
         duration: "2024 - Present",
-        description: "Conducted Request for Quality Inspection (RQI)...",
+        description: "Conducted Request for Quality Inspection (RQI) with the Owner's Inspector to verify construction quality and ensure compliance with project specifications.",
         responsibilities: [
             "Conduct RQI with Owner Inspector",
             "Verify construction quality",
@@ -28,7 +28,7 @@ const projects = [
         role: "Civil Engineer | QA/QC Engineer",
         location: "Sumbawa Barat, Indonesia",
         duration: "2024 - Present",
-        description: "Performed field inspections of construction materials...",
+        description: "Performed field inspections of construction materials, including reinforcing steel, to verify compliance with approved specifications, material approvals (MAR), and project quality requirements. Coordinated with QA/QC and construction personnel to ensure materials were properly identified, stored, and ready for installation.",
         responsibilities: [
             "Inspect reinforcing steel",
             "Verify MAR",
@@ -45,10 +45,10 @@ const projects = [
         image: "assets/project3.jpg",
         project: "AMMAN Process Expansion Project",
         company: "PT Pengembangan Industri Logam",
-        role: "Civil Engineer | QA/QC Engineer",
+        role: "Piping | QA/QC Engineer | Engineer Support",
         location: "Sumbawa Barat, Indonesia",
-        duration: "2024 - Present",
-        description: "Conducted field inspection of seawater and tailing pipeline...",
+        duration: "2025 - Present",
+        description: "Conducted field inspection of the seawater and tailing pipeline to verify installation quality and construction progress.",
         responsibilities: [
             "Inspect pipeline installation",
             "Verify alignment",
@@ -58,4 +58,403 @@ const projects = [
             "assets/project3.jpg"
         ]
     }
+
+ {
+        id: 4,
+        title: "Joint Site Inspection with BVI & Main Contractor",
+        image: "assets/project4.jpg",
+        project: "900 KTPA AMIN Captive Cooper Smelter Project",
+        company: "PT Pengembangan Industri Logam",
+        role: "Civil | QA/QC Engineer",
+        location: "Sumbawa Barat, Indonesia",
+        duration: "2024 - 2025",
+        description: "Conducted a joint site inspection with Bureau Veritas Indonesia (BVI) and the main contractor to verify construction quality and project progress.",
+        responsibilities: [
+            "Coordinate joint inspections with BVI",
+            "Verify construction quality",
+            "Review project specifications",
+            "Document inspection findings",
+            "Coordinate corrective actions"
+        ],
+        gallery: [
+            "assets/project4.jpg"
+        ]
+    },
+
+ {
+        id: 5,
+        title: "Field Coordination with Site Supervisor",
+        image: "assets/project5.jpg",
+        project: "AMMAN Process Expansion Project",
+        company: "PT Pengembangan Industri Logam",
+        role: "Civil | QA/QC Engineer",
+        location: "Sumbawa Barat, Indonesia",
+        duration: "2024 - Present",
+        description: "Conducted field coordination with the site supervisor to review work progress, resolve site issues, and ensure compliance with project requirements.",
+        responsibilities: [
+            "Coordinate with site supervisor",
+            "Review construction progress",
+            "Resolve field issues",
+            "Verify engineering drawings",
+            "Prepare coordination reports"
+        ],
+        gallery: [
+            "assets/project5.jpg"
+        ]
+    },
+
+ {
+        id: 6,
+        title: "Steel Structure Workshop Inspection",
+        image: "assets/project6.jpg",
+        project: "900 KTPA AMIN Captive Cooper Smelter Project",
+        company: "PT Pengembangan Industri Logam",
+        role: "Steel Stucture | QA/QC Engineer | Engineer Support",
+        location: "Sumbawa Barat, Indonesia",
+        duration: "2025 - 2026",
+        description: "Performed site inspection on the steel structure workshop construction to verify structural installation progress, construction quality, and compliance with engineering drawings and project specifications.",
+        responsibilities: [
+            "Inspect reinforcing steel",
+            "Inspect steel structure installation",
+            "Verify construction quality",
+            "Review structural drawings",
+            "Monitor construction progress",
+            "Prepare inspection reports"
+        ],
+        gallery: [
+            "assets/project6.jpg"
+        ]
+    },
+
+ {
+        id: 7,
+        title: "Mechanical & Structural Inspection of Storage Facility",
+        image: "assets/project7.jpg",
+        project: "900 KTPA AMIN Captive Cooper Smelter Project",
+        company: "PT Pengembangan Industri Logam",
+        role: "Mechanical | Steel Structure | QA/QC Engineer",
+        location: "Sumbawa Barat, Indonesia",
+        duration: "2025 - Present",
+        description: "Performed inspection of mechanical equipment installation and structural components inside the industrial storage facility to verify construction quality, installation progress, and compliance with engineering drawings and project specifications.",
+        responsibilities: [
+            "Inspect structural installation",
+            "Verify mechanical equipment installation",
+            "Review engineering drawings",
+            "Monitor construction progress",
+            "Prepare inspection reports"
+            "Verify MAR",
+            "Coordinate with QA/QC"
+        ],
+        gallery: [
+            "assets/project7.jpg"
+        ]
+    },
+
+ {
+        id: 8,
+        title: "Piping & Mechanical Inspection at Seawater Pump Station",
+        image: "assets/project8.jpg",
+        project: "AMMAN Process Expansion Project",
+        company: "PT Pengembangan Industri Logam",
+        role: "Piping | Mechanical | QA/QC Engineer",
+        location: "Sumbawa Barat, Indonesia",
+        duration: "2025 - Present",
+        description: "Performed inspection of piping systems and mechanical equipment at the Seawater Pump Station to verify installation quality, mechanical integrity, and compliance with engineering drawings, technical specifications, and project requirements.",
+        responsibilities: [
+            "Inspect piping installation",
+            "Verify mechanical equipment",
+            "Review technical drawings",
+            "Coordinate with QA/QC team",
+            "Document inspection results"
+        ],
+        gallery: [
+            "assets/project8.jpg"
+        ]
+    },
+
+ {
+        id: 9,
+        title: "QC Coordination for Underground Pipeline Coating",
+        image: "assets/project9.jpg",
+        project: "AMMAN Process Expansion Project",
+        company: "PT Pengembangan Industri Logam",
+        role: "Piping | QA/QC Engineer",
+        location: "Sumbawa Barat, Indonesia",
+        duration: "2026 - Present",
+        description: "Coordinated with the Coating QC team to inspect underground pipeline coating, verify installation quality, and ensure compliance with project specifications before backfilling activities.",
+        responsibilities: [
+            "Coordinate with Coating QC team",
+            "Inspect underground pipeline coating",
+            "Verify coating quality",
+            "Ensure specification compliance",
+            "Support pre-backfilling inspections"
+        ],
+        gallery: [
+            "assets/project9.jpg"
+        ]
+    },
+
+ {
+        id: 10,
+        title: "Industrial Smelter Project Monitoring",
+        image: "assets/project10.jpg",
+        project: "900 KTPA AMIN Captive Cooper Smelter Project",
+        company: "PT Pengembangan Industri Logam",
+        role: "Civil | Steel Structure | QA/QC Engineer",
+        location: "Sumbawa Barat, Indonesia",
+        duration: "2024 - 2025",
+        description: "Conducted site monitoring throughout the industrial smelter construction project to observe construction progress, coordinate field activities, and support engineering and QA/QC documentation.",
+        responsibilities: [
+            "Monitor construction progress",
+            "Coordinate field activities",
+            "Verify work execution",
+            "Support QA/QC documentation",
+            "Prepare daily inspection reports"
+        ],
+        gallery: [
+            "assets/project10.jpg"
+        ]
+    },
+
+ {
+        id: 11,
+        title: "Foundation Construction Inspection",
+        image: "assets/project11.jpg",
+        project: "AMMAN Process Expansion Project",
+        company: "PT Pengembangan Industri Logam",
+        role: "Civil | QA/QC Engineer",
+        location: "Sumbawa Barat, Indonesia",
+        duration: "2024 - Present",
+        description: "Performed field inspection of foundation construction activities, including reinforcement, formwork, and foundation preparation, to ensure compliance with engineering drawings, specifications, and quality standards.",
+        responsibilities: [
+            "Inspect reinforcement installation",
+            "Inspect formwork",
+            "Verify foundation dimensions",
+            "Review engineering drawings",
+            "Ensure compliance with project specifications"
+        ],
+        gallery: [
+            "assets/project11.jpg"
+        ]
+    },
+
+ {
+        id: 12,
+        title: "Shop Drawing & As-Built Drawing Review",
+        image: "assets/project12.jpg",
+        project: "AMMAN Process Expansion Project",
+        company: "PT Pengembangan Industri Logam",
+        role: "Civil | Piping | Steel Structure | QA/QC Engineer",
+        location: "Sumbawa Barat, Indonesia",
+        duration: "2025 - Present",
+        description: "Reviewed shop drawings and as-built drawings using AutoCAD with the site engineering team to verify construction details, resolve drawing discrepancies, and ensure compliance with project requirements.",
+        responsibilities: [
+            "Review shop drawings",
+            "Review as-built drawings",
+            "Verify construction details",
+            "Resolve drawing discrepancies",
+            "Prepare engineering documentation"
+        ],
+        gallery: [
+            "assets/project12.jpg"
+        ]
+    },
+
+ {
+        id: 13,
+        title: "Survey Coordination & Field Verification",
+        image: "assets/project13.jpg",
+        project: "AMMAN Process Expansion Project",
+        company: "PT Pengembangan Industri Logam",
+        role: "Civil | QA/QC Engineer",
+        location: "Sumbawa Barat, Indonesia",
+        duration: "2024 - Present",
+        description: "Coordinated with the survey team during field measurements and verification using Total Station to ensure construction layout, alignment, and elevation complied with engineering drawings and project specifications.",
+        responsibilities: [
+            "Coordinate with survey team",
+            "Verify construction layout",
+            "Check alignment and elevation",
+            "Review survey data",
+            "Support field engineering activities"
+        ],
+        gallery: [
+            "assets/project13.jpg"
+        ]
+    },
+
+ {
+        id: 15,
+        title: "Acid Storage Tank Inspection",
+        image: "assets/project15.jpg",
+        project: "900 KTPA AMIN Captive Cooper Smelter Project",
+        company: "PT Pengembangan Industri Logam",
+        role: "Mechanical | QA/QC Engineer",
+        location: "Sumbawa Barat, Indonesia",
+        duration: "2024 - 2025",
+        description: "Conducted field inspections of the Acid Storage Tank area to verify structural conditions, civil works, and installation quality, ensuring compliance with engineering specifications and project standards.",
+        responsibilities: [
+            "Inspect acid storage tank area",
+            "Verify structural conditions",
+            "Inspect civil construction works",
+            "Ensure specification compliance",
+            "Prepare inspection documentation"
+        ],
+        gallery: [
+            "assets/project15.jpg"
+        ]
+    },
+
+ {
+        id: 16,
+        title: "Instrumentation Field Inspection",
+        image: "assets/project16.jpg",
+        project: "AMMAN Process Expansion Project",
+        company: "PT Pengembangan Industri Logam",
+        role: "Instrument | QA/QC Engineer",
+        location: "Sumbawa Barat, Indonesia",
+        duration: "2026 - Present",
+        description: "Performed field inspection and verification of instrumentation devices with the Instrument team to ensure proper installation, accessibility, and compliance with engineering drawings and project specifications.",
+        responsibilities: [
+            "Inspect instrumentation devices",
+            "Verify installation quality",
+            "Coordinate with instrumentation team",
+            "Check accessibility and installation",
+            "Support QA/QC verification"
+        ],
+        gallery: [
+            "assets/project16.jpg"
+        ]
+    },
+
+ {
+        id: 17,
+        title: "Primary Crusher Facility in Mining Project",
+        image: "assets/project17.jpg",
+        project: "AMNT Mining Project",
+        company: "PT Pengembangan Industri Logam",
+        role: "Civil | Steel Structure | QA/QC Engineer",
+        location: "Sumbawa Barat, Indonesia",
+        duration: "2025 - 2026",
+        description: "Performed field inspections at the Primary Crusher facility to verify structural, architectural, and civil construction work, ensuring compliance with engineering drawings, project specifications, and quality standards.",
+        responsibilities: [
+            "Inspect Primary Crusher civil works",
+            "Verify structural installation",
+            "Review engineering drawings",
+            "Monitor construction quality",
+            "Prepare inspection documentation"
+        ],
+        gallery: [
+            "assets/project17.jpg"
+        ]
+    },
+
+ {
+        id: 18,
+        title: "De-Aeration Box Civil & Structural Inspection",
+        image: "assets/project18.jpg",
+        project: "AMMAN Process Expansion Project",
+        company: "PT Pengembangan Industri Logam",
+        role: "Civil | Steel Stucture | QA/QC Engineer",
+        location: "Sumbawa Barat, Indonesia",
+        duration: "2024 - Present",
+        description: "Performed field inspections of the De-Aeration Box structure to verify civil construction quality, structural installation, and compliance with approved engineering drawings and project specifications.",
+        responsibilities: [
+            "Inspect De-Aeration Box structure",
+            "Verify civil construction quality",
+            "Review structural drawings",
+            "Coordinate with construction team",
+            "Prepare inspection reports"
+        ],
+        gallery: [
+            "assets/project18.jpg"
+        ]
+    },
+
+ {
+        id: 19,
+        title: "Site Problem Solving & Engineering Coordination",
+        image: "assets/project19.jpg",
+        project: "AMMAN Process Expansion Project",
+        company: "PT Pengembangan Industri Logam",
+        role: "Engineer | QA/QC Engineer",
+        location: "Sumbawa Barat, Indonesia",
+        duration: "2024 - Present",
+        description: "Collaborated with the Owner's Inspector and site engineers to investigate field issues, verify construction conditions against engineering drawings, and determine appropriate technical solutions to support project execution.",
+        responsibilities: [
+            "Investigate field issues",
+            "Coordinate with Owner's Inspector",
+            "Review engineering drawings",
+            "Recommend technical solutions",
+            "Support engineering coordination"
+        ],
+        gallery: [
+            "assets/project19.jpg"
+        ]
+    },
+
+ {
+        id: 20,
+        title: "Weekly Coordination Meeting",
+        image: "assets/project20.jpg",
+        project: "AMMAN Process Expansion Project",
+        company: "PT Pengembangan Industri Logam",
+        role: "Engineer | QA/QC Engineer",
+        location: "Sumbawa Barat, Indonesia",
+        duration: "2024 - Present",
+        description: "Participated in weekly coordination meetings to review project progress, discuss technical issues, monitor construction activities, and coordinate actions among engineering, QA/QC, and construction teams.",
+        responsibilities: [
+            "Attend weekly coordination meetings",
+            "Review project progress",
+            "Discuss technical issues",
+            "Coordinate engineering and QA/QC activities",
+            "Prepare meeting documentation"
+        ],
+        gallery: [
+            "assets/project20.jpg"
+        ]
+    },
+
+ {
+        id: 21,
+        title: "AutoCAD Civil & Structural Drawings",
+        image: "assets/project21.png",
+        project: "AMMAN Process Expansion Project",
+        company: "PT Pengembangan Industri Logam",
+        role: "Engineer | QA/QC Engineer",
+        location: "Sumbawa Barat, Indonesia",
+        duration: "2024 - Present",
+        description: "Developed and updated civil and structural as-built drawings for construction projects using AutoCAD. Ensured drawing accuracy based on field modifications, coordinated revisions with engineering teams, and prepared final documentation for project completion.",
+        responsibilities: [
+            "Prepare AutoCAD drawings",
+            "Update as-built drawings",
+            "Review engineering revisions",
+            "Verify field dimensions",
+            "Prepare final documentation"
+        ],
+        gallery: [
+            "assets/project21.png"
+        ]
+    },
+
+ {
+        id: 22,
+        title: "Engineering Document Handover Package",
+        image: "assets/project22.jpg",
+        project: "AMMAN Process Expansion Project",
+        company: "PT Pengembangan Industri Logam",
+        role: "Document Control | QA/QC Engineer",
+        location: "Sumbawa Barat, Indonesia",
+        duration: "2024 - Present",
+        description: "anaged and compiled engineering handover documentation, including as-built drawings, inspection records, material approval records (MAR), welding documentation, QA/QC certificates, and project completion dossiers to support final project handover.",
+        responsibilities: [
+            "Prepare handover packages",
+            "Compile engineering documents",
+            "Manage QA/QC records",
+            "Review as-built documentation",
+            "Coordinate final project submission"
+        ],
+        gallery: [
+            "assets/project22.jpg"
+        ]
+    },
 ];
